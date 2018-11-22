3788d38f39e4595d5374f222cea31cdcdec294d2
Kalydy Márk <Mark Kalydy>
2018-11-22 12:36:59 +0100

    adding POJOs. Closes #2

---
+++ src/main/java/com/springframework/sfpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springframework.sfpetclinic.model;
+
+public class Owner  extends  Person{
+}
---
+++ src/main/java/com/springframework/sfpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springframework.sfpetclinic.model;
+
+public class Vet extends Person {
+}
