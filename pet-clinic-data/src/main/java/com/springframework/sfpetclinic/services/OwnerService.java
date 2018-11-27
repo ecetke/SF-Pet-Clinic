922ffc46f836bd192141fb4356becc6037de885f
ecetke <markiiikaa@hotmail.com>
2018-11-27 16:47:24 +0100

    Implement interfaces for services. Closes #5

--- pet-clinic-data/src/main/java/com/springframework/sfpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/springframework/sfpetclinic/services/OwnerService.java
@@ -6,8 +6,11 @@ import java.util.Set;
 
 public interface OwnerService {
 
-Owner findByLastName(String lastName);
+    Owner findByLastName(String lastName);
+
     Owner findById(Long id);
+
+    Owner findByFirstName(String firstName);
 
     Owner save(Owner owner);
 
