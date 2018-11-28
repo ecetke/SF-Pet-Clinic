e2f172543ff600482432ac4e1afca9521c021d31
ecetke <markiiikaa@hotmail.com>
2018-11-28 17:21:13 +0100

    a

--- pet-clinic-web/src/main/java/com/springframework/sfpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfpetclinic/controllers/OwnerController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
 
-    @RequestMapping({"/owner",  "owner/index.html", "owner/index","owner/owners.html"})
+    @RequestMapping({"/", "/index.html", "/index", "/owners.html"})
     public String index() {
-        return "index";
+        return "owners/index";
     }
 }
--- pet-clinic-web/src/main/java/com/springframework/sfpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfpetclinic/controllers/VetController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 
-    @RequestMapping({"","/vets","vets/index","/vets/index.html"})
+    @RequestMapping({"","/","/index.html"})
     public String index(){
-        return "/vets/index";
+        return  "vets/index";
     }
 
 }
