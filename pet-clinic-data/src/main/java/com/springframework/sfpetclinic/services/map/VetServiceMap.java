0506c0e5487b159d03d212e9f465157b9856d570
ecetke <markiiikaa@hotmail.com>
2018-11-28 13:14:21 +0100

    Delete some files..

---
+++ pet-clinic-data/src/main/java/com/springframework/sfpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.springframework.sfpetclinic.services.map;
+
+import com.springframework.sfpetclinic.model.Vet;
+import com.springframework.sfpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return null;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+}
